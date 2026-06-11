b1137dca72a581067a78d452529ff2f72ad67874
salsa <salsabilarafifahh@gmail.com>
2026-06-11 08:50:06 +0700

    deploy supabase

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,12 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
+// Pola Singleton untuk mencegah banyak instance PrismaClient di mode pengembangan
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['query', 'error', 'warn'], // Opsional: untuk membantu debugging
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
